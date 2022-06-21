ea7396f1c59c77f70f067a6ebcff5d1addd804d6
alexzrp <87832147+alexzrp@users.noreply.github.com>
2022-06-21 18:52:24 -0300

    Added ratelimit test

--- test/rateLimit.spec.js
+++ test/rateLimit.spec.js
@@ -4,17 +4,22 @@ const client = require('../src/http/client');
 describe('Rate limit', () => {
   beforeAll(async () => {
     nock('http://test.local')
-    .get('/user')
-    .reply(429, null, { 'rate-limit-reset': 1 })
-    .get('/user')
-    .reply(200, { ok: true });
+    .get('/user?id=1')
+    .reply(429, null, { 'rate-limit-reset': '1' })
+    .get('/user?id=1')
+    .reply(200, { id: 1 });
   });
 
   describe('GET /user', () => {
     it('responds with json', async () => {
       const request = new client({ apiKey: '123' });
-      const { data } = await request.get('http://test.local/user');
-      expect(data).toStrictEqual({ ok: true });
+      const retrySpy = jest.spyOn(request.axios, 'request');
+      const { data } = await request.axios.request('http://test.local/user', { params: { id: 1 } });
+      expect(data).toStrictEqual({ id: 1 });
+      expect(retrySpy).toBeCalledTimes(2);
+      expect(retrySpy).toBeCalledWith('http://test.local/user', { params: { id: 1 }, url: 'http://test.local/user' });
+      expect(retrySpy).toBeCalledWith('http://test.local/user', { params: { id: 1 }, url: 'http://test.local/user' });
+
     });
   });
 });
